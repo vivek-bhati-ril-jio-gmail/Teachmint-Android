9e0e1448e0034c40c14bd7650620a08c34b42fca
Vivek Bhati <vivek.bhati.ril.jio@gmail.com>
2024-10-20 01:50:19 +0530

    reverted changes back

--- app/src/main/java/com/vivek/githubassignment/utils/Constants.kt
+++ app/src/main/java/com/vivek/githubassignment/utils/Constants.kt
@@ -3,8 +3,6 @@ package com.vivek.githubassignment.utils
 object Constants {
     const val BASE_URL = "https://api.github.com/"
     const val GITHUB_API_VERSION = "2022-11-28"
-    private const val TOKEN_P1 = "github_pat_11A7FMKGY0mtzl5U03rDgj_WvxttZEMkoOYGzr"
-    private const val TOKEN_P2 = "FOWQzqmQgvjqaEMYVr1MN8W37VgK5HMR5KRJax8trb53"
-    const val AUTHORIZATION_TOKEN = "Bearer $TOKEN_P1$TOKEN_P2"
+    const val AUTHORIZATION_TOKEN = "Bearer FOWQzqmQgvjqaEMYVr1MN8W37VgK5HMR5KRJax8trb53github_pat_11A7FMKGY0mtzl5U03rDgj_WvxttZEMkoOYGzr"
     const val ACCEPT_HEADER = "application/vnd.github+json"
 }
