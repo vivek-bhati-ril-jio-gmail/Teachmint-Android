46143e31c624fc47d8b79c8df3fd1fa71cb9c7aa
vivek-bhati-ril-jio-gmail <130729243+vivek-bhati-ril-jio-gmail@users.noreply.github.com>
2024-10-20 00:31:07 +0530

    Update Constants.kt

--- app/src/main/java/com/vivek/githubassignment/utils/Constants.kt
+++ app/src/main/java/com/vivek/githubassignment/utils/Constants.kt
@@ -3,6 +3,6 @@ package com.vivek.githubassignment.utils
 object Constants {
     const val BASE_URL = "https://api.github.com/"
     const val GITHUB_API_VERSION = "2022-11-28"
-    const val AUTHORIZATION_TOKEN = "Bearer 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"
+    const val AUTHORIZATION_TOKEN = "Bearer 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"
     const val ACCEPT_HEADER = "application/vnd.github+json"
 }
