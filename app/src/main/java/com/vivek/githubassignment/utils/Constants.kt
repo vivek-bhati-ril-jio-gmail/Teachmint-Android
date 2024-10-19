bde40ff1929974df29789a8cc89de97d0c8ad65c
vivek-stacks <vivekbhati036@hotmail.com>
2024-10-20 01:27:27 +0530

    apk changed commit

--- app/src/main/java/com/vivek/githubassignment/utils/Constants.kt
+++ app/src/main/java/com/vivek/githubassignment/utils/Constants.kt
@@ -3,6 +3,8 @@ package com.vivek.githubassignment.utils
 object Constants {
     const val BASE_URL = "https://api.github.com/"
     const val GITHUB_API_VERSION = "2022-11-28"
-    const val AUTHORIZATION_TOKEN = "Bearer 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"
+    private const val TOKEN_P1 = "github_pat_11A7FMKGY0mtzl5U03rDgj_WvxttZEMkoOYGzr"
+    private const val TOKEN_P2 = "FOWQzqmQgvjqaEMYVr1MN8W37VgK5HMR5KRJax8trb53"
+    const val AUTHORIZATION_TOKEN = "Bearer $TOKEN_P1$TOKEN_P2"
     const val ACCEPT_HEADER = "application/vnd.github+json"
 }
